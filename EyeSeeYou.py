af011b117155e102373c7d92c2668d93362b9853
breakfix <37887458+breakfix@users.noreply.github.com>
2021-04-30 11:49:52 +1000

    Update EyeSeeYou.py

--- EyeSeeYou.py
+++ EyeSeeYou.py
@@ -4,8 +4,15 @@ import time
 import os
 import sys
 
-host = "127.0.0.1"
-port = "1080"
+print("""\
+    ______          _____       __  __           
+   / ____/_  _____ / ___/___  __\ \/ /___  __  __
+  / __/ / / / / _ \\__ \/ _ \/ _ \  / __ \/ / / /
+ / /___/ /_/ /  __/__/ /  __/  __/ / /_/ / /_/ / 
+/_____/\__, /\___/____/\___/\___/_/\____/\__,_/  
+      /____/                                  
+              v0.2 Matt Johnson @breakfix  
+ """)
 
 def path(value):
     # Make path absoloute
@@ -18,12 +25,12 @@ parser.add_argument("-i", dest="input_file", required=True, type=path,
                     help='File being outputted by Masscan to use as input file')
 parser.add_argument("--headless", dest="headless", action='store_true',
                     help='Run Chrome in headless mode')
-parser.add_argument("--no-masscan", dest="format", action='store_true',
-                    help='Read targets from comma separated input file instead of a running masscan (IP,PORT)')
-parser.add_argument("--timeout", dest="timeout",
+parser.add_argument("--no-masscan", dest="format",
+                    help='Read targets from input file instead of running masscan (requires you specify ports to use as argument e.g. --no-masscan 80,443,8080)')
+parser.add_argument("--timeout", dest="timeout", default=3,
                     help='Set timeout in seconds (default is 3 seconds)')
-parser.add_argument("--proxy", dest="proxy", action='store_true',
-                    help='Use socks5 proxy (default is 127.0.0.1:1080')
+parser.add_argument("--proxy", dest="proxy", default='127.0.0.1:1080', const='127.0.0.1:1080', nargs='?',
+                    help='Use socks4 proxy (default is 127.0.0.1:1080) optionally provide a socks host and port e.g. --proxy 192.168.1.1:8080')
 
 # Print help if no arguments given
 if len(sys.argv)==1:
@@ -38,44 +45,40 @@ if args.headless:
     chrome_options.headless = True
 
 if args.proxy:
-    chrome_options.add_argument("--proxy-server=socks5://" + host + ":" + port);
+    chrome_options.add_argument("--proxy-server=socks4://" + args.proxy)
 
 driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=chrome_options) 
 driver.set_window_size(1024, 768) # set the window size that you need 
-
-if args.timeout:
-    driver.set_page_load_timeout(int(args.timeout))
-else:
-    driver.set_page_load_timeout(3)
+driver.set_page_load_timeout(int(args.timeout))
 
 targets = []
 
 if args.format:
     with open(args.input_file, "r") as infile:
+        ports = args.format.split(',')
         for line in infile:
-            entry = line.split(',')
-            ip = entry[0]
-            port = entry[1]
-            target = ip + ":" + port
-            if target not in targets:
-                print("Fetching " + "http://" + ip + ":" + port)
-                try:
-                    driver.get('http://' + ip + ":" + port)
-                    time.sleep(1) # wait for page to fully load
-                    driver.save_screenshot(os.path.join(args.out_dir, str(ip) + ':' + str(port) + '_http.png'))
-                    targets.append(ip + ":" + port)
-                except:
-                    print("[!] Something went wrong")
-                    targets.append(ip + ":" + port)                
-                print("Fetching " + "https://" + ip + ":" + port)
-                try:
-                    driver.get('https://' + ip + ":" + port)
-                    time.sleep(1)
-                    driver.save_screenshot(os.path.join(args.out_dir, str(ip) + ':' + str(port) + '_https.png'))
-                    targets.append(ip + ":" + port)
-                except:
-                    print("[!] Something went wrong")
-                    targets.append(ip + ":" + port)
+            for port in ports:
+                target = line.rstrip('\r\n') + ":" + port
+                ip = line.rstrip('\r\n')
+                if target not in targets:
+                    print("Fetching " + "http://" + ip + ":" + port)
+                    try:
+                        driver.get('http://' + ip + ":" + port)
+                        time.sleep(1) # wait for page to fully load
+                        driver.save_screenshot(os.path.join(args.out_dir, str(ip) + ':' + str(port) + '_http.png'))
+                        targets.append(ip + ":" + port)
+                    except:
+                        print("[!] Something went wrong")
+                        targets.append(ip + ":" + port)                
+                    print("Fetching " + "https://" + ip + ":" + port)
+                    try:
+                        driver.get('https://' + ip + ":" + port)
+                        time.sleep(1)
+                        driver.save_screenshot(os.path.join(args.out_dir, str(ip) + ':' + str(port) + '_https.png'))
+                        targets.append(ip + ":" + port)
+                    except:
+                        print("[!] Something went wrong")
+                        targets.append(ip + ":" + port)
 else:
     while True:
         with open(args.input_file, "r") as infile:
